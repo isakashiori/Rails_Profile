ccb45c9bcb00276e885eb0de52fe2a8f80f50042
isakashiori <isslamdunk0000@gmail.com>
2022-09-23 10:37:52 +0900

    add:アップローダーの追加

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,27 @@
 class ProfilesController < ApplicationController
+  before_action :set_user, only: %i[edit update]
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to profile_path, success: "ユーザーを登録しました"
+    else
+      flash.now['danger'] = "ユーザー登録が失敗しました"
+      render :edit
+    end
+  end
+
+  def show; end
+
+  private
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :last_name, :first_name, :avatar, :avatar_cache)
+  end
 end
+
